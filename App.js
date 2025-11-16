38f4738fa5bc13d24aa319c39c793af35f55f7f2
suraj gupta <sandygupta0414@gmail.com>
2025-11-16 10:59:36 +0530

    day2 done

--- App.js
+++ App.js
@@ -14,19 +14,44 @@
 //     </div>
 // </div> 
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  [React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "Hello World from React"),
-    React.createElement("h1", {}, "Welcome to React"),
-  ]),React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "Hello World from React"),
-    React.createElement("h1", {}, "Welcome to React"),
+  [React.createElement("div", { id: "child",key:'child1' }, [
+    React.createElement("h1", { id: "heading", key: "h1a" }, "Hello World from React"),
+    React.createElement("h1", {key: "h1b"}, "Welcome to React"),
+  ]),React.createElement("div", { id: "child2" ,key:'child2'}, [
+    React.createElement("h1", { id: "heading" ,key: "h2a"}, "Hello World from React"),
+    React.createElement("h1", {key: "h2b"}, "Welcome to React"),
   ])]
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   [
+//     React.createElement("div", { id: "child", key: "child1" }, [
+//       React.createElement("h1", { className: "heading", key: "h1a" }, "Hello World from React"),
+//       React.createElement("h2", { key: "h2a" }, "Welcome to React"),
+//     ]),
+//     React.createElement("div", { id: "child2", key: "child2" }, [
+//       React.createElement("h1", { className: "heading", key: "h1b" }, "Hello World from React"),
+//       React.createElement("h2", { key: "h2b" }, "Welcome to React"),
+//     ])
+//   ]
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
